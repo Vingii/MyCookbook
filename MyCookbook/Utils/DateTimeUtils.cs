52ba096b938bfbc6b2a9432ae7c499d3b7e236e6
Vingii <50549345+Vingii@users.noreply.github.com>
2024-06-11 14:54:39 +0200

    Improve duration formatting

---
+++ MyCookbook/Utils/DateTimeUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCookbook.Utils
+{
+    public static class DateTimeUtils
+    {
+        public static int? AsMinutes(string s)
+        {
+            s = s.Trim().Replace("h ", ":").Replace(" ", "").Replace("h:", ":").Replace("h", "").Replace("m", "");
+            if (int.TryParse(s, out var minutes)) return minutes;
+            if (TimeSpan.TryParse(s, out var timeSpan)) return (int)timeSpan.TotalMinutes;
+            return null;
+        }
+    }
+}
